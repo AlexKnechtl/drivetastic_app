6141c3d2347812dd24e7ed2ca698ac7ee6241a34
Fabio Moretti <fmoretti981@gmail.com>
2019-08-27 11:51:59 +0200

    New Question Implementation

--- src/views/Learning/Question.tsx
+++ src/views/Learning/Question.tsx
@@ -1,38 +1,36 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, ScrollView, Dimensions } from 'react-native';
 import { QuestionPicture, QuestionLayout, Answer, QuestionFAB, TransparentLayout, ImageZoom } from 'components';
+import * as core from "core";
 import { Bild } from 'img';
 import { icons } from 'icons';
 import ImageViewer from 'react-native-image-zoom-viewer';
 //@ts-ignore
 import { InstagramProvider, ElementContainer } from "@postillon/react-native-instagram-zoomable";
 import { TouchThroughWrapper, TouchThroughView } from "react-native-touch-through-view";
+
+interface State{
+    answerStates: {selected: boolean}[],
+    imageZoom: boolean,
+    shouldValidate: boolean
+}
+
 class Question extends Component {
-    state = {
-        answer1Pressed: false,
-        answer2Pressed: false,
-        answer3Pressed: false,
-        answer4Pressed: false,
-        imageZoom: false
-    }
-
-    answer1Pressed() {
-        this.setState({ answer1Pressed: !this.state.answer1Pressed })
-    }
-
-    answer2Pressed() {
-        this.setState({ answer2Pressed: !this.state.answer2Pressed })
-    }
-
-    answer3Pressed() {
-        this.setState({ answer3Pressed: !this.state.answer3Pressed })
-    }
-
-    answer4Pressed() {
-        this.setState({ answer4Pressed: !this.state.answer4Pressed })
+    state: State = {
+        answerStates: [],
+        imageZoom: false,
+        shouldValidate: false
     }
 
     render() {
+        const question = new core.Question("Wie werden Sie sich hier verhalten?", "ID: 01", [
+            new core.Answer("Ich muss hier anhalten", false),
+            new core.Answer("Bis zu den Personen fahre ich auf Gefahrensicht", true),
+            new core.Answer("Ich muss hier aufgrund der Kinder mein Tempo auf Schrittgeschwindigkeit reduzieren und den Dick in die Hand nehmen.", false),
+            new core.Answer("Noch eine geile Antwort", false)
+        ], 2);
+        if(this.state.answerStates.length < question.answers.length)
+            this.setState({answerStates: question.answers.map(a=> ({selected: false}))});
         return (
             <View style={styles.view}>
                 <InstagramProvider>
@@ -45,10 +43,17 @@ class Question extends Component {
                             <TouchThroughView style={{ height: Dimensions.get('window').width * 0.66, width: "100%" }} />
                             <QuestionLayout count={1} text="Wie werden Sie sich hier verhalten?" difficulty="Leicht" />
                             <View style={{ flex: 1, padding: 14, backgroundColor: '#fff' }}>
-                                <Answer text="Ich muss hier anhalten" />
-                                <Answer text="Bis zu den Personen fahre ich auf Gefahrensicht" />
-                                <Answer text="Ich muss hier aufgrund der Kinder mein Tempo auf Schrittgeschwindigkeit reduzieren und den Dick in die Hand nehmen." />
-                                <Answer text="Antwort 4" />
+                                {question.answers.map((a, i)=>
+                                    <Answer key={i} 
+                                        answer={a} 
+                                        selected={this.state.answerStates[i]&&this.state.answerStates[i].selected}
+                                        shouldValidate={this.state.shouldValidate}
+                                        onPress={()=> {
+                                            var {answerStates} = this.state;
+                                            answerStates[i].selected = !answerStates[i].selected;
+                                            this.setState({answerStates});
+                                        }}/>
+                                    )}
                             </View>
                             {/* <View style={{ height: Dimensions.get('window').width, width: 20, backgroundColor: "#ff0" }} /> */}
                         </ScrollView>
