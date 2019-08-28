2025011790ef59945b2bda7e84fc891690880b24
Alexander Knechtl <alex.knechtl@gmx.at>
2019-08-28 08:54:49 +0200

    Added Style to Answer

--- src/components/specific/Answer.tsx
+++ src/components/specific/Answer.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 import { colors } from 'base';
 import { IAnswer } from 'core';
+import { CircleCheck, False } from 'icons/indexHelper';
+
 
 type HeadlineProps = {
     answer: IAnswer,
@@ -10,11 +12,36 @@ type HeadlineProps = {
     onPress: () => void
 }
 
-const Answer = ({ onPress, selected = false, shouldValidate = false, answer }: HeadlineProps) => {
+const Answer = ({ onPress, selected = false, shouldValidate, answer }: HeadlineProps) => {
+    let background = colors.questionBG, icon = undefined, borderWidth = 0, fontColor = "#000", test = true;
+
+    if (selected) {
+        background = colors.questionSelcted;
+        if (answer.isRight && shouldValidate) {
+            background = colors.questionRight;
+            fontColor = "white";
+            icon = CircleCheck;
+        } else if (!answer.isRight && shouldValidate) {
+            background = colors.questionWrong;
+            fontColor = '#fff';
+            icon = False;
+        }
+    } else {
+        background = colors.questionBG;
+        if (answer.isRight && shouldValidate) {
+            background = colors.questionLightGreen;
+            fontColor = colors.questionGreenAccent;
+            borderWidth = 1;
+        } else if (!answer.isRight && shouldValidate) {
+            background = colors.questionLightRed;
+            fontColor = colors.questionRedAccent;
+        }
+    }
+
     return (
-        <TouchableOpacity onPress={onPress} activeOpacity={0.6} style={{ ...styles.view, }}>
-            <Image style={styles.icon} source={undefined} />
-            <Text style={{ ...styles.text,  flex: 1, alignSelf: "center" }}>{answer.answer}</Text>
+        <TouchableOpacity onPress={onPress} activeOpacity={0.6} style={{ ...styles.view, backgroundColor: background, borderWidth: borderWidth }}>
+            <Image style={styles.icon} source={icon} />
+            <Text style={{ ...styles.text, color: fontColor }}>{answer.answer}</Text>
         </TouchableOpacity>
     );
 };
@@ -30,11 +57,13 @@ const styles = StyleSheet.create({
     },
     text: {
         fontSize: 14,
-        textAlign: "left"
+        textAlign: "left",
+        flex: 1,
+        alignSelf: "center"
     },
     icon: {
-        width: 30,
-        height: 30,
+        width: 24,
+        height: 24,
         marginLeft: 12,
         alignSelf: "center"
     }
--- src/views/Learning/Question.tsx
+++ src/views/Learning/Question.tsx
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, ScrollView, Dimensions } from 'react-native';
-import { QuestionPicture, QuestionLayout, Answer, QuestionFAB, TransparentLayout, ImageZoom } from 'components';
+import { QuestionPicture, QuestionLayout, Answer, QuestionFAB } from 'components';
 import * as core from "core";
 import { Bild } from 'img';
 import { icons } from 'icons';
-import ImageViewer from 'react-native-image-zoom-viewer';
 //@ts-ignore
 import { InstagramProvider, ElementContainer } from "@postillon/react-native-instagram-zoomable";
 import { TouchThroughWrapper, TouchThroughView } from "react-native-touch-through-view";
 import { IQuestion } from 'core';
 import { useTranslation } from 'react-i18next';
 
-interface State{
-    answerStates: {selected: boolean}[],
+interface State {
+    answerStates: { selected: boolean }[],
     imageZoom: boolean,
     shouldValidate: boolean
 }
@@ -25,14 +24,14 @@ export class Question extends Component {
     }
 
     render() {
-        const question:IQuestion = new core.Question("Wie werden Sie sich hier verhalten?",3981, [
+        const question: IQuestion = new core.Question("Wie werden Sie sich hier verhalten?", 3981, [
             new core.Answer("Ich muss hier anhalten", false),
             new core.Answer("Bis zu den Personen fahre ich auf Gefahrensicht", true),
             new core.Answer("Ich muss hier aufgrund der Kinder mein Tempo auf Schrittgeschwindigkeit reduzieren und den Dick in die Hand nehmen.", false),
             new core.Answer("Noch eine geile Antwort", false)
         ], 2, "B", 4, "Hard");
-        if(this.state.answerStates.length < question.answers.length)
-            this.setState({answerStates: question.answers.map(a=> ({selected: false}))});
+        if (this.state.answerStates.length < question.answers.length)
+            this.setState({ answerStates: question.answers.map(a => ({ selected: false })) });
         return (
             <View style={styles.view}>
                 <InstagramProvider>
@@ -45,23 +44,22 @@ export class Question extends Component {
                             <TouchThroughView style={{ height: Dimensions.get('window').width * 0.66, width: "100%" }} />
                             <QuestionLayout count={question.Id} text={question.question} difficulty={question.difficulty} />
                             <View style={{ flex: 1, padding: 14, backgroundColor: '#fff' }}>
-                                {question.answers.map((a, i)=>
-                                    <Answer key={i} 
-                                        answer={a} 
-                                        selected={this.state.answerStates[i]&&this.state.answerStates[i].selected}
+                                {question.answers.map((a, i) =>
+                                    <Answer key={i}
+                                        answer={a}
+                                        selected={this.state.answerStates[i] && this.state.answerStates[i].selected}
                                         shouldValidate={this.state.shouldValidate}
-                                        onPress={()=> {
-                                            var {answerStates} = this.state;
+                                        onPress={() => {
+                                            var { answerStates } = this.state;
                                             answerStates[i].selected = !answerStates[i].selected;
-                                            this.setState({answerStates});
-                                        }}/>
-                                    )}
+                                            this.setState({ answerStates });
+                                        }} />
+                                )}
                             </View>
-                            {/* <View style={{ height: Dimensions.get('window').width, width: 20, backgroundColor: "#ff0" }} /> */}
                         </ScrollView>
                     </TouchThroughWrapper>
                     <View style={styles.fabContainer}>
-                        <QuestionFAB icon={icons.Continue} />
+                        <QuestionFAB onPress={() => this.setState({ shouldValidate: true })} icon={icons.Continue} />
                         <QuestionFAB marginRight={12} iconSize={20} size={40} icon={icons.Continue} />
                     </View>
                 </InstagramProvider>
@@ -72,7 +70,8 @@ export class Question extends Component {
 
 const styles = StyleSheet.create({
     view: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#fff'
     },
     fabContainer: {
         flexDirection: "row-reverse",
