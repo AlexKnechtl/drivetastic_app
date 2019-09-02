14f749387a94c0ee6ecdefbbce3a5fcd458fd3f9
Fabio Moretti <fmoretti981@gmail.com>
2019-09-02 20:52:06 +0200

    Test with More mock modules

--- src/core/providers/MockQuestionProvider.ts
+++ src/core/providers/MockQuestionProvider.ts
@@ -1,8 +1,9 @@
 import { IQuestionProvider } from "./IQuestionProvider";
 import { Question, ModuleTypes, Answer } from "core/entities";
 
-const settings = {
-    modules: "B",
+const settings: {modules: ModuleTypes[], questionCountPerSection: number,
+    sectionCount: number} = {
+    modules: ["B", "G", "C"],
     questionCountPerSection: 20,
     sectionCount: 10
 }
@@ -18,17 +19,20 @@ function generateRandomIntBetween(start: number, end: number){
 export class MockQuestionProvider implements IQuestionProvider{
     async loadQuestions(): Promise<Question[]> {
         var qs:Question[] = [];
-        for (let sId = 0; sId < settings.sectionCount; sId++) {
-            for (let i = 0; i < settings.questionCountPerSection; i++) {
-                var rand = Math.random();
-                var answers = Array.apply(null, Array(4)).map(a=> Math.random()).map((a,i)=> new Answer("Answer"+(i+1), a>0.5));
-                qs.push(new Question("QuestionText"+i, 
-                    (i+sId*settings.questionCountPerSection).toString(),
-                    answers,
-                    generateRandomIntBetween(-3, 6),
-                    settings.modules as ModuleTypes, 
-                    settings.modules+sId, 
-                    rand<0.5?"Easy":rand>0.8?"Hard":"Medium"));
+        for (let mID = 0; mID < settings.modules.length; mID++) {
+            const module = settings.modules[mID];
+            for (let sId = 0; sId < settings.sectionCount; sId++) {
+                for (let i = 0; i < settings.questionCountPerSection; i++) {
+                    var rand = Math.random();
+                    var answers = Array.apply(null, Array(4)).map(a=> Math.random()).map((a,i)=> new Answer("Answer"+(i+1), a>0.5));
+                    qs.push(new Question("QuestionText"+i, 
+                        (i+sId*settings.questionCountPerSection + mID*settings.questionCountPerSection*settings.sectionCount).toString(),
+                        answers,
+                        generateRandomIntBetween(-3, 6),
+                        module, 
+                        module+sId, 
+                        rand<0.5?"Easy":rand>0.8?"Hard":"Medium"));
+                }
             }
         }
         return qs;
