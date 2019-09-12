342afc8b60e3cf756f821c9edef7936b1ade2771
Fabio Moretti <fmoretti981@gmail.com>
2019-09-12 16:57:34 +0200

    fix

--- src/views/App/ExamView.tsx
+++ src/views/App/ExamView.tsx
@@ -6,37 +6,31 @@ import { NavigationScreenProps, ScrollView } from 'react-navigation';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { ThemeContext } from 'base';
 
-class ExamView extends Component<NavigationScreenProps> {
-    constructor(props: any) {
-        super(props);
-    }
-
-    render() {
-        const colors = useContext(ThemeContext);
-        return (
-            <ScrollView style={styles.safeArea}>
-                <ColoredIconHeader text="Prüfung" color={colors.lightPurple} icon={icons.Exam} />
-                <Text style={styles.title}>Module</Text>
-                <View style={{ marginHorizontal: 14 }}>
-                    {/* TODO: Implement */}
-                    <LongIconButton title="Alle Bereiche" text="Volle Prüfungssimulation" bgColor={colors.lightPurple} icon={icons.Test} onPress={()=> null} />
-                    <IconButton onPress={() => this.props.navigation.navigate("TrainingView")} color={colors.grundwissen} icon={icons.Grundwissen} text="Grundwissen" />
-                    <IconButton onPress={() => this.props.navigation.navigate("ExamView")} color={colors.bFührerschein} icon={icons.Car} text="Bereich B" />
-                </View>
-                <Text style={styles.title}>Beste Ergebnisse</Text>
-                <BestResult title="Grundwissen" color={colors.grundwissen} score={0.2} />
-                <BestResult title="Bereich B" color={colors.bFührerschein} score={0.2} />
-                <View style={styles.historyView}>
-                    <Text style={{ color: "white", textAlign: "center", fontSize: 18, marginBottom: 14 }}>Prüfungshistorie</Text>
-                    <ModuleProgress text1="Grundwissen" percentage={0.5} />
-                    <ModuleProgress text1="B Bereich" percentage={0.2} />
-                    <TouchableOpacity onPress={() => this.props.navigation.navigate("Statistics")} style={styles.button}>
-                        <Text style={[styles.buttonText, {color: colors.lightPurple}]}>Mehr erfahren</Text>
-                    </TouchableOpacity>
-                </View>
-            </ScrollView>
-        )
-    }
+const ExamView = ({ navigation }: NavigationScreenProps) => {
+    const colors = useContext(ThemeContext);
+    return (
+        <ScrollView style={styles.safeArea}>
+            <ColoredIconHeader text="Prüfung" color={colors.lightPurple} icon={icons.Exam} />
+            <Text style={styles.title}>Module</Text>
+            <View style={{ marginHorizontal: 14 }}>
+                {/* TODO: Implement */}
+                <LongIconButton title="Alle Bereiche" text="Volle Prüfungssimulation" bgColor={colors.lightPurple} icon={icons.Test} onPress={() => null} />
+                <IconButton onPress={() => navigation.navigate("TrainingView")} color={colors.grundwissen} icon={icons.Grundwissen} text="Grundwissen" />
+                <IconButton onPress={() => navigation.navigate("ExamView")} color={colors.bFührerschein} icon={icons.Car} text="Bereich B" />
+            </View>
+            <Text style={styles.title}>Beste Ergebnisse</Text>
+            <BestResult title="Grundwissen" color={colors.grundwissen} score={0.2} />
+            <BestResult title="Bereich B" color={colors.bFührerschein} score={0.2} />
+            <View style={styles.historyView}>
+                <Text style={{ color: "white", textAlign: "center", fontSize: 18, marginBottom: 14 }}>Prüfungshistorie</Text>
+                <ModuleProgress text1="Grundwissen" percentage={0.5} />
+                <ModuleProgress text1="B Bereich" percentage={0.2} />
+                <TouchableOpacity onPress={() => navigation.navigate("Statistics")} style={styles.button}>
+                    <Text style={[styles.buttonText, { color: colors.lightPurple }]}>Mehr erfahren</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    )
 }
 
 const styles = StyleSheet.create({
